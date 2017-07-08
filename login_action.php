a3ae76b9da3356d775ae44955276146c700a43d9
Shaun Dashjian <shaundashjian@yahoo.com>
2017-07-08 17:32:06 -0600

    Added login tools, action, and basic home page

---
+++ login_action.php
@@ -0,0 +1,21 @@
+<?php
+  if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    require('connect_db.php');
+    require('login_tools.php');
+    list($check, $data) =
+      validate($dbc, $_POST['email'], $_POST['password']);
+
+    if($check){
+      session_start();
+      $_SESSION['id'] = $data['id'];
+      $_SESSION['first_name'] = $data['first_name'];
+      $_SESSION['last_name'] = $data['last_name'];
+      load('home.php');
+
+    } else {
+      $errors = $data;
+    }
+    mysqli_close($dbc);
+  }
+  include('login.php');
+ ?>
---
+++ login_tools.php
@@ -0,0 +1,36 @@
+<?php
+  function load($page = 'login.php'){
+    $url = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
+    $url = rtrim($url, '/\\');
+    $url .= '/'.$page;
+    header("Location: $url");
+    exit();
+  }
+ function validate($dbc, $email = '', $password = '') {
+   $errors = array();
+   if(empty($email)) {
+     $errors[] = 'Enter your email address.';
+   } else {
+     $email = mysqli_real_escape_string($dbc, trim($email));
+   }
+   if(empty($password)) {
+     $errors[] = 'Enter your password.';
+   } else {
+     $password = mysqli_real_escape_string($dbc, trim($password));
+   }
+
+   if(empty($error)){
+    $query = "SELECT id, first_name, last_name
+              FROM user WHERE email = '$email'
+              AND password = SHA1('$password')";
+      $result = mysqli_query($dbc, $query);
+      if(mysqli_num_rows($result) == 1){
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        return array(true, $row);
+      } else {
+        $errors[] = 'Email adress and password not found';
+
+      }
+   }
+   return array(false, $errors);
+ }
